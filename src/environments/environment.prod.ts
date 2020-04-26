23210bd3000bd5e3bf9a170b8670046f982e3dc2
Shizz74 <wojciechbajko@gmail.com>
2020-04-26 12:14:22 +0200

    check git ignore

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAFyxqQA0F3wxR8_nftdrmBW614o50uCws",
+    authDomain: "prod-cook-chef.firebaseapp.com",
+    databaseURL: "https://prod-cook-chef.firebaseio.com",
+    projectId: "prod-cook-chef",
+    storageBucket: "prod-cook-chef.appspot.com",
+    messagingSenderId: "818862017870",
+    appId: "1:818862017870:web:244af28cb9ed347b8ce21f"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyABzK_Om4Opf3j-QTuUTb5bx98hE14wgkc",
+    authDomain: "cook-book-test-142db.firebaseapp.com",
+    databaseURL: "https://cook-book-test-142db.firebaseio.com",
+    projectId: "cook-book-test-142db",
+    storageBucket: "cook-book-test-142db.appspot.com",
+    messagingSenderId: "999938982912",
+    appId: "1:999938982912:web:9489f64f1773dec375c4e5"
+  }
 };
 
 /*
