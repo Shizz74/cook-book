9593068756747a5b78778c9bbf3c18eb40d872f4
Shizz74 <wojciechbajko@gmail.com>
2020-06-24 22:20:47 +0200

    changes on menu. change styles, chagne app display and clearing code

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, NgModule } from '@angular/core';
+import { Component, OnInit, Input, NgModule } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { AuthService } from '../auth/auth.service';
 
 
 @Component({
@@ -9,11 +11,22 @@ import { Component, OnInit, NgModule } from '@angular/core';
 
 
 export class MenuComponent implements OnInit {
-
+  title = 'angular-9-i18n';
+  langs = ['en', 'pl'];
   
-  constructor() { }
+  constructor(private translateService: TranslateService, public authService: AuthService) { }
 
   ngOnInit(): void {
+    let browserlang = this.translateService.getBrowserLang();
+   if (this.langs.indexOf(browserlang) > -1) {
+     this.translateService.setDefaultLang(browserlang);
+   } else {
+     this.translateService.setDefaultLang('en');
+   }
+  }
+
+  public useLanguage(lang: string): void {
+    this.translateService.setDefaultLang(lang);
   }
 
 }
